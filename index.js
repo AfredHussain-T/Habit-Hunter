a65ef8d80a0bc5e74f15eb55b909b5a6369e16c2
AfredHussain-T <afredhussain69@gmail.com>
2023-08-07 18:44:20 +0530

    We are done with the comments

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// Importing express
 const express = require('express');
+// The port on which our server is running
 const port = 7000;
 const app = express();
+// Importing data base connection
 const db = require('./config/mongoose');
+// Importing the router file
 const router = require('./routes/index');
 
-
+//Middleware is used to parse the data from the incoming request
 app.use(express.urlencoded());
 app.use(express.static('./assets'));
 app.use('/' , router);
