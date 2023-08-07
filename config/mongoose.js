3d4ab90aa3db0dff6a58205765d672b6f58f8583
AfredHussain-T <afredhussain69@gmail.com>
2023-08-07 16:21:49 +0530

    Added comments in mongoose configuration

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); //Importing mongoose
 
+// Establishing database connection
 mongoose.connect('mongodb://127.0.0.1:27017/habit-hunter');
 
 const mongoConnection = mongoose.connection;
 
+// This will show the error if any while connecting the Data base
 mongoConnection.on('error' , console.error.bind(console, 'There is some issue while connecting to DB '));
+
 
 mongoConnection.once('open' , function(){
     console.log('Mongo Connected successfully');
