88b90d19b4ffc1dd4687d0865df618e7ccb0ed50
AfredHussain-T <108061234+AfredHussain-T@users.noreply.github.com>
2023-08-29 16:47:32 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose'); //Importing mongoose
 
 // Establishing database connection
 // mongoose.connect('mongodb://127.0.0.1:27017/habit-hunter');
-mongoose.connect('mongodb+srv://afredhussain69:Rova7nxwtw1zl18Q@cluster0.r9drjvc.mongodb.net/Habit-Tracker');
-
+// mongoose.connect('mongodb+srv://afredhussain69:Rova7nxwtw1zl18Q@cluster0.r9drjvc.mongodb.net/Habit-Tracker');
+mongoose.connect(process.env.MONGODB_CONNECT_URI);
 const mongoConnection = mongoose.connection;
 
 // This will show the error if any while connecting the Data base
